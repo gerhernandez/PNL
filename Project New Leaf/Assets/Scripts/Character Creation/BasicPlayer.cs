a06ae2203416aae1a32e8a8d623349d640c2b447
Jessica Ubaldo <jubaldo@csumb.edu>
2019-02-02 21:48:10 -0800

    from character to character branch: BasicPlayer.cs and Player.cs added and modified

---
+++ Project New Leaf/Assets/Scripts/Character Creation/BasicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IAction<T>
+{
+    void Action(T action);
+}
+
+public interface IDirectionalMove<T>
+{
+    void Move(T direction);
+}
+
+public interface IChangeStatus<T>
+{
+    void ChangeStatus(T status);
+}
---
+++ Project New Leaf/Assets/Scripts/Character Creation/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour, IAction<Input>, IDirectionalMove<Input>, IChangeStatus<int>
+{
+    // variables
+    public string playerName;
+    public int healthPoints;
+    public int magicPoints;
+
+    // essential attributes will go here
+
+    // cosmetics
+    private int hair;
+    private int eyes;
+    private int top;
+    private int bottom;
+    private int footwear;
+
+    public void Action(Input action)
+    {
+        // do action based on Input
+    }    
+    
+    public void Move(Input direction)
+    {
+        // go in direction based on Input
+    }
+
+    public void ChangeStatus(int status)
+    {
+        // change given status
+    }
+
+}
