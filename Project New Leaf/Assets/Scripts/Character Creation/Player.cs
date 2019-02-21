b4b01ee7cece5833410e424187b3a942bd333477
Jessica Ubaldo <jubaldo@csumb.edu>
2019-02-20 22:18:05 -0800

    edited Player.cs; added a Test player and Ground for movement;

--- Project New Leaf/Assets/Scripts/Character Creation/Player.cs
+++ Project New Leaf/Assets/Scripts/Character Creation/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour, IAction<Input>, IChangeStatus<int>, IAbilities<int>
+public class Player : MonoBehaviour, IAction<string>, IChangeStatus<int>, IAbilities<int>
 {
     // variables
     public string playerName;
@@ -20,16 +20,44 @@ public class Player : MonoBehaviour, IAction<Input>, IChangeStatus<int>, IAbilit
 
     // instance of Player
 
-    public void Action(Input action)
+    /** TODO */
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log("getting pressed\n");
+        Move("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space pressed\n");
+            Action("jump");
+        }
+    }
+
+    public void Action(string act)
     {
         // do action based on Input
+        // for jumping
+        if (act.Equals("jump"))
+        {
+            Debug.Log("Jump entered\n");
+            rb.AddForce(transform.position * 2);
+        }
     }    
     
-    /*
-    public void Move(Input direction)
+    public void Move(string dir)
     {
         // go in direction based on Input
-    }*/
+        Debug.Log("Input: " + Input.GetAxis(dir) + "\n");
+        Debug.Log("Direction: " + dir + "\n");
+        rb.AddForce(transform.right * Input.GetAxis(dir) * 2);
+    }
 
     public void ChangeStatus(int status)
     {
