940e0f78ad665022cdf34d3b38d39ff1f1cfd7d5
connmatt <matt.connolly1225@gmail.com>
2019-02-27 21:39:15 -0800

    started the character creation scene

--- Project New Leaf/Assets/Scripts/Character Creation/CharacterCreation.cs
+++ Project New Leaf/Assets/Scripts/Character Creation/CharacterCreation.cs
@@ -1,32 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterCreation : CharacterAttributes {
 	
+	public Text hairText;
+	public Text topsText;
+	public Text headText;
+	public Text bodyText;
+	public Text bottomsText;
+	public Text lowerText;
+
 	CharacterAttributes charAttributes;
 
 	GameObject characterAttributes;
 
 	// Use this for initialization
 	void Start () {
+
+		hairText.text = cosmetics["hair"].GetValue(0).ToString();
 		
-		foreach(KeyValuePair<string, int[]> item in cosmetics)
-		{
-			for(int i = 0; i < item.Value.Length; i++)
-			{
-				Debug.Log(item.Key + " " + item.Value.GetValue(i));
-			}
-			for(int i = 0; i < item.Value.Length; i++)
-			{
-				if(item.Key == "race")
-				{
-					Debug.Log(races[i]);
-				}
+		// foreach(KeyValuePair<string, int[]> item in cosmetics)
+		// {
+		// 	for(int i = 0; i < item.Value.Length; i++)
+		// 	{
+		// 		Debug.Log(item.Key + " " + item.Value.GetValue(i));
+		// 	}
+		// 	for(int i = 0; i < item.Value.Length; i++)
+		// 	{
+		// 		if(item.Key == "race")
+		// 		{
+		// 			Debug.Log(races[i]);
+		// 		}
 				
-			}
+		// 	}
 			
-		}
+		// }
 	}
 	
 	// Update is called once per frame
