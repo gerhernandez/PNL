61bc3cb688dbe60a052e838aa1f2ab91f35cd112
coregalado@csumb.edu <coregalado>
2019-03-04 13:12:16 -0800

    finished basic enemy movement

--- Project New Leaf/Assets/Scripts/EnemyAI/EnemyMovement.cs
+++ Project New Leaf/Assets/Scripts/EnemyAI/EnemyMovement.cs
@@ -7,55 +7,62 @@ public class EnemyMovement : MonoBehaviour {
 	public LayerMask enemyMask; //this is used to check isGrounded
 	public Transform myTrans;
 	private float myWidth;
+    private float myHeight;
 
 	public float movementspeed;
 	private Rigidbody2D rb;
-	private bool facing = true;
 	private float horizontal;
-	//Look into Collider, Bounds, and extents
+    //Look into Collider, Bounds, and extents
 
-	void Start(){
+    //This bool is used to determine direction the sprite is facing.
+    //This is to be assigned in the inspector during scene editing.
+    //True represents facing right, false represents facing left.
+    [SerializeField] private bool facing;
+
+    void Start(){
 		if(movementspeed == 0f){
 			movementspeed = 5f;
 		}
 		myTrans = this.transform;
 		rb = GetComponent<Rigidbody2D>();
-		myWidth = this.GetComponent<SpriteRenderer>().bounds.extents.x;
+        SpriteRenderer mySprite = this.GetComponent<SpriteRenderer>();
 
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
-	}
+        myWidth = mySprite.bounds.extents.x;
+        myHeight = mySprite.bounds.extents.y;
+
+        if (!facing)
+        {
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
 
 	void FixedUpdate () {
 		moveInBounds();
 	}
 
 	void moveInBounds(){
-		Vector2 lineCastPos = myTrans.position - myTrans.right * myWidth;
-		
-		Debug.DrawLine(lineCastPos, lineCastPos + Vector2.down*4);
-		bool isGrounded = Physics2D.Linecast(lineCastPos, lineCastPos + Vector2.down*4, enemyMask);
-		
-		if(!isGrounded){	
-			flip();
-		}
+        Vector2 lineCastPos = myTrans.position - myTrans.right*1.1f * myWidth;
 
-		Vector2 myVel = rb.velocity;
-		myVel.x = myTrans.right.x * -movementspeed * Time.deltaTime;
-		rb.velocity = myVel;
-	}
+        Debug.DrawLine(lineCastPos, lineCastPos + Vector2.down * 3);
+        bool isGrounded = Physics2D.Linecast(lineCastPos, lineCastPos + Vector2.down * 3, enemyMask);
 
-	//Flip the direction that the sprite is facing
-	private void flip(){
-		facing = !facing;
-		Vector3 currRot = myTrans.eulerAngles;
-		if(facing){
-			currRot.y += 180;
-		} else {
-			currRot.y -= 180;
-		}
-		
-		myTrans.eulerAngles = currRot;
-	}
+        Vector2 rightTrans = new Vector2(myTrans.right.x, myTrans.right.y);
+        Debug.DrawLine(lineCastPos, lineCastPos - rightTrans * 0.2f);
+        bool isBlocked = Physics2D.Linecast(lineCastPos, lineCastPos - rightTrans * 0.2f, enemyMask);
+
+        //If there's no ground, or if I'm blocked I should turn around.
+        if (!isGrounded || isBlocked)
+        {
+            Vector3 currRot = myTrans.eulerAngles;
+            currRot.y += 180;
+            myTrans.eulerAngles = currRot;
+        }
+
+        Vector2 myVel = rb.velocity;
+        myVel.x = -myTrans.right.x * movementspeed;
+        rb.velocity = myVel;
+    }
+
 }
