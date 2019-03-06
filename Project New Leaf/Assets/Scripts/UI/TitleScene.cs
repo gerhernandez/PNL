cf79be0f90af260fe777df5642ed21ec1ea1a828
eric tinoco <ertinoco@csumb.edu>
2019-03-06 15:44:15 -0800

    Added event options to TitleScene Script

--- Project New Leaf/Assets/Scripts/UI/TitleScene.cs
+++ Project New Leaf/Assets/Scripts/UI/TitleScene.cs
@@ -3,26 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class TitleScene : MonoBehaviour {
 
-    public Button playButton;
-    public Button controlsButton;
-    public Button creditsButton;
-    public Button exitGameButton;
-    public Button backControlsButton;
-    public Button backCreditsButton;
+    public GameObject playButton;
+    public GameObject controlsButton;
+    public GameObject creditsButton;
+    public GameObject exitGameButton;
+    public GameObject backControlsButton;
+    public GameObject backCreditsButton;
     public GameObject creditsPanel;
     public GameObject controlsPanel;
+    public EventSystem eventSystem;
 
     private void Awake()
     {
-        playButton.onClick.AddListener(delegate { play("TemporaryScene"); });
-        controlsButton.onClick.AddListener(delegate { controls(); });
-        creditsButton.onClick.AddListener(delegate { credits(); });
-        exitGameButton.onClick.AddListener(delegate { exit(); });
-        backControlsButton.onClick.AddListener(delegate { back(); });
-        backCreditsButton.onClick.AddListener(delegate { back(); });
+        playButton.GetComponent<Button>().onClick.AddListener(delegate { play("CharacterCreation"); });
+        controlsButton.GetComponent<Button>().onClick.AddListener(delegate { controls(); });
+        creditsButton.GetComponent<Button>().onClick.AddListener(delegate { credits(); });
+        exitGameButton.GetComponent<Button>().onClick.AddListener(delegate { exit(); });
+        backControlsButton.GetComponent<Button>().onClick.AddListener(delegate { back(); });
+        backCreditsButton.GetComponent<Button>().onClick.AddListener(delegate { back(); });
+        eventSystem.SetSelectedGameObject(playButton);
     }
 
     public void play(string scene)
@@ -32,10 +35,12 @@ public class TitleScene : MonoBehaviour {
     public void controls()
     {
         controlsPanel.SetActive(true);
+        eventSystem.SetSelectedGameObject(backControlsButton);
     }
     public void credits()
     {
         creditsPanel.SetActive(true);
+        eventSystem.SetSelectedGameObject(backCreditsButton);
     }
     public void exit()
     {
@@ -46,5 +51,6 @@ public class TitleScene : MonoBehaviour {
     {
         controlsPanel.SetActive(false);
         creditsPanel.SetActive(false);
+        eventSystem.SetSelectedGameObject(playButton);
     }
 }
