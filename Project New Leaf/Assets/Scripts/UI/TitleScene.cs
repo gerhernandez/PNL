273a635fbdf91624982a1d28cdab30632db09339
eric tinoco <ertinoco@csumb.edu>
2019-03-06 17:55:56 -0800

    Added temp pause menu options and scene, also, Added new input choice in project settings called HorizontalX

--- Project New Leaf/Assets/Scripts/UI/TitleScene.cs
+++ Project New Leaf/Assets/Scripts/UI/TitleScene.cs
@@ -7,46 +7,68 @@ using UnityEngine.EventSystems;
 
 public class TitleScene : MonoBehaviour {
 
-    public GameObject playButton;
-    public GameObject controlsButton;
-    public GameObject creditsButton;
-    public GameObject exitGameButton;
-    public GameObject backControlsButton;
-    public GameObject backCreditsButton;
-    public GameObject creditsPanel;
-    public GameObject controlsPanel;
-    public EventSystem eventSystem;
+    public GameObject playButton;           // To hold the play button
+    public GameObject controlsButton;       // To hold the control button
+    public GameObject creditsButton;        // To hold the credits button
+    public GameObject exitGameButton;       // To hold the exit game button
+    public GameObject backControlsButton;   // To hold the back button in controls
+    public GameObject backCreditsButton;    // To hold the back button in credits
+    public GameObject creditsPanel;         // To hold the credits panel
+    public GameObject controlsPanel;        // To hold the controls panel
+    public EventSystem eventSystem;         // To hold the Event System object
 
     private void Awake()
     {
+        // **** Set all the listeners for each button ****
         playButton.GetComponent<Button>().onClick.AddListener(delegate { play("CharacterCreation"); });
         controlsButton.GetComponent<Button>().onClick.AddListener(delegate { controls(); });
         creditsButton.GetComponent<Button>().onClick.AddListener(delegate { credits(); });
         exitGameButton.GetComponent<Button>().onClick.AddListener(delegate { exit(); });
         backControlsButton.GetComponent<Button>().onClick.AddListener(delegate { back(); });
         backCreditsButton.GetComponent<Button>().onClick.AddListener(delegate { back(); });
+
+        // Set the current choice to the continue button
         eventSystem.SetSelectedGameObject(playButton);
     }
 
+    /// <summary>
+    /// Start Game: Loads the first scene (Prologue)
+    /// </summary>
+    /// <param name="scene"> Prologue scene </param>
     public void play(string scene)
     {
         SceneManager.LoadScene(scene);
     }
+
+    /// <summary>
+    /// Controls Panel: Shows an image or directions of the controls of the game.
+    /// </summary>
     public void controls()
     {
         controlsPanel.SetActive(true);
         eventSystem.SetSelectedGameObject(backControlsButton);
     }
+
+    /// <summary>
+    /// Credits Panel: Shows the name of people who created the game
+    /// </summary>
     public void credits()
     {
         creditsPanel.SetActive(true);
         eventSystem.SetSelectedGameObject(backCreditsButton);
     }
+
+    /// <summary>
+    /// Exit Game
+    /// </summary>
     public void exit()
     {
         Application.Quit();
     }
 
+    /// <summary>
+    /// Goes back from the panel to the main menu
+    /// </summary>
     public void back()
     {
         controlsPanel.SetActive(false);
