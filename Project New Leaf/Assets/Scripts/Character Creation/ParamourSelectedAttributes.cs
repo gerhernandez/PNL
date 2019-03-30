19317a314ce70248cb9d5a10987cc5015f77405d
Jessica Ubaldo <jubaldo@csumb.edu>
2019-03-29 19:07:17 -0700

    Created PlayerSelectedAttributes.cs and ParamourSelectedAttributes.cs

---
+++ Project New Leaf/Assets/Scripts/Character Creation/ParamourSelectedAttributes.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParamourSelectedAttributes : MonoBehaviour {
+    // all things static
+    // Sprites
+    private static SpriteRenderer loveSelectedHair;
+    private static SpriteRenderer loveSelectedSkin;
+    private static SpriteRenderer loveSelectedShirt;
+    private static SpriteRenderer loveSelectedPants;
+    private static SpriteRenderer loveSelectedLineart;
+    private static SpriteRenderer loveSelectedSkinShading;
+    private static SpriteRenderer loveSelectedClothShading;
+
+    // colors of Sprites
+    private static Color loveSelectedSkinColor;
+    private static Color loveSelectedHairColor;
+    private static Color loveSelectedShirtColor;
+    private static Color loveSelectedPantsColor;
+
+    // int
+    private static int loveSelectedSkinPos = 0;
+    private static int loveSelectedHairPos = 0;
+    private static int loveSelectedSkinColorPos = 0;
+    private static int loveSelectedHairColorPos = 0;
+    private static int loveSelectedShirtColorPos = 0;
+    private static int loveSelectedPantsColorPos = 0;
+    private static int loveSelectedCisOrTransInt = 0;
+    private static int loveSelectedPronounInt = 0;
+
+    public SpriteRenderer LoveSelectedHair
+    { get; set; }
+
+    public SpriteRenderer LoveSelectedSkin
+    { get; set; }
+
+    public SpriteRenderer LoveSelectedShirt
+    { get; set; }
+
+    public SpriteRenderer LoveSelectedPants
+    { get; set; }
+
+    public SpriteRenderer LoveSelectedLineart
+    { get; set; }
+
+    public SpriteRenderer LoveSelectedSkinShading
+    { get; set; }
+
+    public SpriteRenderer LoveSelectedClothShading
+    { get; set; }
+
+    public Color LoveSelectedSkinColor
+    { get; set; }
+
+    public Color LoveSelectedHairColor
+    { get; set; }
+
+    public Color LoveSelectedShirtColor
+    { get; set; }
+
+    public Color LoveSelectedPantsColor
+    { get; set; }
+
+    public int LoveSelectedSkinPos
+    { get; set; }
+
+    public int LoveSelectedHairPos
+    { get; set; }
+
+    public int LoveSelectedSkinColorPos
+    { get; set; }
+
+    public int LoveSelectedHairColorPos
+    { get; set; }
+
+    public int LoveSelectedShirtColorPos
+    { get; set; }
+
+    public int LoveSelectedPantsColorPos
+    { get; set; }
+
+    public int LoveSelectedCisOrTransInt
+    { get; set; }
+
+    public int LoveSelectedPronounInt
+    { get; set; }
+}
---
+++ Project New Leaf/Assets/Scripts/Character Creation/PlayerSelectedAttributes.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelectedAttributes : MonoBehaviour {
+    // all things static
+    // Sprites
+    private static SpriteRenderer playSelectedHair;
+    private static SpriteRenderer p1aySelectedSkin;
+    private static SpriteRenderer p1aySelectedShirt;
+    private static SpriteRenderer p1aySelectedPants;
+    private static SpriteRenderer playSelectedLineart;
+    private static SpriteRenderer playSelectedSkinShading;
+    private static SpriteRenderer playSelectedClothShading;
+
+    // colors of Sprites
+    private static Color playSelectedSkinColor;
+    private static Color playSelectedHairColor;
+    private static Color playSelectedShirtColor;
+    private static Color playSelectedPantsColor;
+
+    // int
+    private static int playSelectedSkinPos = 0;
+    private static int playSelectedHairPos = 0;
+    private static int playSelectedSkinColorPos = 0;
+    private static int playSelectedHairColorPos = 0;
+    private static int playSelectedShirtColorPos = 0;
+    private static int playSelectedPantsColorPos = 0;
+    private static int playSelectedCisOrTransInt = 0;
+    private static int playSelectedPronounInt = 0;
+
+    public SpriteRenderer PlaySelectedHair
+    { get; set; }
+
+    public SpriteRenderer PlaySelectedSkin
+    { get; set; }
+
+    public SpriteRenderer PlaySelectedShirt
+    { get; set; }
+
+    public SpriteRenderer PlaySelectedPants
+    { get; set; }
+
+    public SpriteRenderer PlaySelectedLineart
+    { get; set; }
+
+    public SpriteRenderer PlaySelectedSkinShading
+    { get; set; }
+
+    public SpriteRenderer PlaySelectedClothShading
+    { get; set; }
+
+    public Color PlaySelectedSkinColor
+    { get; set; }
+
+    public Color PlaySelectedHairColor
+    { get; set; }
+
+    public Color PlaySelectedShirtColor
+    { get; set; }
+
+    public Color PlaySelectedPantsColor
+    { get; set; }
+
+    public int PlaySelectedSkinPos
+    { get; set; }
+
+    public int PlaySelectedHairPos
+    { get; set; }
+
+    public int PlaySelectedSkinColorPos
+    { get; set; }
+
+    public int PlaySelectedHairColorPos
+    { get; set; }
+
+    public int PlaySelectedShirtColorPos
+    { get; set; }
+
+    public int PlaySelectedPantsColorPos
+    { get; set; }
+
+    public int PlaySelectedCisOrTransInt
+    { get; set; }
+
+    public int PlaySelectedPronounInt
+    { get; set; }
+}
