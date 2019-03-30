848e4fc54495efda352ad8ae115808f10f3c1846
connmatt <matt.connolly1225@gmail.com>
2019-03-29 19:05:49 -0700

    added a back button and a confirmation for character creation

---
+++ Project New Leaf/Assets/Scripts/Character Creation/CharacterConfirmation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterConfirmation : MonoBehaviour {
+
+	public Canvas popUpCanvas;
+	// Use this for initialization
+	void Start () {
+		popUpCanvas.enabled = false;
+	}
+	
+	public void popUpConfirmation(){
+		popUpCanvas.enabled = true;
+	}
+
+	public void Confirmation(Button btn){
+		if(btn.name.Equals("YesBtn")){
+			//create player
+		}
+		else{
+			popUpCanvas.enabled = false;
+		}
+	}
+}
