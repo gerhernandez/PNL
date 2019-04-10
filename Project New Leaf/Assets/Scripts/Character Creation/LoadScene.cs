3bb16134d22257b192a0e5079a78ec96b3eb18cb
Jessica Ubaldo <jubaldo@csumb.edu>
2019-04-09 20:51:25 -0700

    Many changes for Character Creation: scripts and scenes changed to test Fungus dialogue

---
+++ Project New Leaf/Assets/Scripts/Character Creation/LoadScene.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+    public bool loadScene;
+    public string sceneName;
+
+	// Use this for initialization
+	void Start () {
+        loadScene = false;
+        sceneName = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (loadScene)
+        {
+            StartCoroutine(LoadAsyncScene());
+        }
+	}
+
+    public void SetAndLoadScene(string s)
+    {
+        sceneName = s;
+        loadScene = true;
+    }
+
+    IEnumerator LoadAsyncScene()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
