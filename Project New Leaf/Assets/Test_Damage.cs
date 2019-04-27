e4917fcb9a3b7c4ea04afd59f4a8b117909c545c
Jessica Ubaldo <jubaldo@csumb.edu>
2019-04-27 00:20:03 -0700

    future edit: rename Test_AnimationControl to AnimationControl; deleted TestImages folder; added tags

--- Project New Leaf/Assets/Test_Damage.cs
+++ Project New Leaf/Assets/Test_Damage.cs
@@ -13,50 +13,10 @@ public class Test_Damage : MonoBehaviour {
         p = FindObjectOfType<Player>();
         sr = GetComponent<SpriteRenderer>();
         isDamaged = false;
-        dmgTime = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		/*if (p.isDamaged)
-        {
-            DamageOverTime(dmgTime);
-            dmgTime += Time.deltaTime;
-        }
-        else
-        {
-            dmgTime = 0;
-        }*/
-
         sr.color = Color.Lerp(Color.white, Color.red, Mathf.PingPong(Time.time, 1f));
 	}
-
-    /*
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.name == "Player")
-        {
-            isDamaged = true;
-            p.isDamaged = true;
-        }
-    }
-
-
-    void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.name == "Player")
-        {
-            isDamaged = false;
-            p.isDamaged = false;
-        }
-    }
-    */
-    /*void DamageOverTime(float dmgTime)
-    {
-        Debug.Log("time: " + (int)dmgTime % 2);
-        if ((int) dmgTime % 2 == 0)
-        {
-            Debug.Log("Damage!! at " + (int) dmgTime % 2);
-        }
-    }*/
 }
