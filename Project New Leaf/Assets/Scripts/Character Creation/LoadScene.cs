92c5ddbf3cd5b1c679398d931598eed1b0921b79
connmatt <matt.connolly1225@gmail.com>
2019-05-03 18:45:36 -0700

    created a loading canvas to use when a scene is being loaded

--- Project New Leaf/Assets/Scripts/Character Creation/LoadScene.cs
+++ Project New Leaf/Assets/Scripts/Character Creation/LoadScene.cs
@@ -7,9 +7,16 @@ public class LoadScene : MonoBehaviour {
     public bool loadScene;
     public string sceneName;
 
+    public Animation loadingAnimation;
+
+    public Canvas loadingCanvas;
+
 	// Use this for initialization
 	void Start () {
-     
+        loadingAnimation = GameObject.Find("LoadingCanvas").GetComponentInChildren<Animation>();
+        loadingCanvas = GameObject.Find("LoadingCanvas").GetComponent<Canvas>();
+        loadingCanvas.enabled = false;
+        loadingAnimation.Stop("Loading Leaf");
 	}
 
     public void SetAndLoadScene(string s)
@@ -23,8 +30,18 @@ public class LoadScene : MonoBehaviour {
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
         while (!asyncLoad.isDone)
         {
+            //Loading Canvas displayed
+            loadingCanvas.enabled = true;
+            loadingAnimation.Play("Loading Leaf");
+
             yield return null;
         }
+        loadingCanvas.enabled = false;
+        if(loadingCanvas.enabled == true && loadingAnimation.IsPlaying("Loading Leaf") ==  true){
+            loadingCanvas.enabled = false;
+            loadingAnimation.Stop("Loading Leaf");
+        }
+        
     }
 
     public string SceneName
