7d58359465c8931151f2e9d4ebcb1919e6f17d13
tinoco86 <ertinoco@csumb.edu>
2019-05-13 18:53:59 -0700

    Added a new script called portrait script that will create the portraits of the player and paramour

---
+++ Project New Leaf/Assets/Scripts/PortraitScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortraitScript : MonoBehaviour {
+
+    public GameManager gameManager;
+    public int resWidth;
+    public int resHeight;
+
+    private Camera portraitCamera;
+
+    private void Awake()
+    {
+        portraitCamera = GetComponent<Camera>();
+        StartCoroutine(TakeScreenShot());
+    }
+
+    private IEnumerator TakeScreenShot()
+    {
+        yield return new WaitForEndOfFrame();
+
+        RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+        portraitCamera.targetTexture = rt;
+        Texture2D _screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+        portraitCamera.Render();
+        RenderTexture.active = rt;
+        _screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+        _screenShot.Apply();
+        portraitCamera.targetTexture = null;
+        RenderTexture.active = null;
+        Destroy(rt);
+        Sprite tempSprite = Sprite.Create(_screenShot, new Rect(0, 0, resWidth, resHeight), new Vector2(0, 0));
+
+        //if(portraitCamera.name == "PlayerCamera")
+        //{
+        gameManager.SetPlayerPortrait(tempSprite);
+        //}
+        //else if(portraitCamera.name == "ParamourCamera")
+        //{
+        //    gameManager.SetParamourPortrait(tempSprite);
+        //}
+
+    }
+}
