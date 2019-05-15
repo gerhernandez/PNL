35864d04417f7932cbb6cb8a8273eef64ec1199a
Cody Regalado <coregalado@csumb.edu>
2019-05-14 18:58:11 -0700

    disabling mouseInput starting from Title Menu; preventing player from pausing while loading; small adjustments to player prefab

---
+++ Project New Leaf/Assets/mouseInputSuppressant.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class mouseInputSuppressant : MonoBehaviour {
+
+    private static mouseInputSuppressant instance = null;
+    GameObject lastselect;
+
+    // Use this for initialization
+    void Start () {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        
+        Cursor.visible = false;
+        lastselect = new GameObject();
+    }
+	
+	// Update is called once per frame
+	void Update () { 
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            EventSystem.current.SetSelectedGameObject(lastselect);
+        }
+        else
+        {
+            lastselect = EventSystem.current.currentSelectedGameObject;
+        }
+    }
+}
