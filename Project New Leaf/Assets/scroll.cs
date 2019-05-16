4e28eb11dfdb3abe3720ab4a6b06cab28990ab2a
connmatt <matt.connolly1225@gmail.com>
2019-05-15 21:27:45 -0700

    Fixed the bug about the hair not looking the same as the hair the user selects

--- Project New Leaf/Assets/scroll.cs
+++ Project New Leaf/Assets/scroll.cs
@@ -3,17 +3,22 @@ using System.Collections;
 
 public class scroll : MonoBehaviour {
 
-public float speed = 0.5f;
+	public float speed = -2.0f;
+	GameObject mountains;
+	Vector2 startPos;
 
 	// Use this for initialization
 	void Start () {
-	
+		startPos = transform.position;
+		mountains = GameObject.Find("Nighttime_Bg_Mountains");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector2 offset = new Vector2(Time.time * speed, 0);
+		float newPos = Mathf.Repeat(Time.time * speed, 50);
+		transform.position = startPos + Vector2.right * newPos;
+		// Vector2 offset = new Vector2(Time.time * speed, 0);
 		
-		GetComponent<Renderer>().material.mainTextureOffset = offset;
+		// GetComponent<Renderer>().material.mainTextureOffset = offset;
 	}
 }
