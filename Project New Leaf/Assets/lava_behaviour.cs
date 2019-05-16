7e9739d9ce8f5b33ea676e6a64b1117d1fba7e15
Cody Regalado <coregalado@csumb.edu>
2019-05-16 12:35:59 -0700

    updating lava audio, added fadeout to lava based on distance

--- Project New Leaf/Assets/lava_behaviour.cs
+++ Project New Leaf/Assets/lava_behaviour.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class lava_behaviour : MonoBehaviour {
 
 	public GameObject player;
+    public AudioSource audio;
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag("Player");
+        audio = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        //This is used to calculate how loud our 
+        //lava should be depending on how far away the player is
+        var t = (transform.position - player.transform.position);
+        float newT = Vector3.SqrMagnitude(t);
+        newT = (newT - Mathf.Pow(audio.minDistance, 2) / Mathf.Pow(audio.maxDistance, 2));
+        audio.spatialBlend = Mathf.Lerp(10f, 22000f, newT);
+    }
 
 	private void OnTriggerEnter2D(Collider2D other)
 	{
