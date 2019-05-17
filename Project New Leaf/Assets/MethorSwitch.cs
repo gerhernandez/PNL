e22240c942721a3ed60222e8ec0394cc5e725823
Jessica Ubaldo <jubaldo@csumb.edu>
2019-05-16 23:10:42 -0700

    script created for mentorSwitch

---
+++ Project New Leaf/Assets/MethorSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MethorSwitch : MonoBehaviour {
+
+    public bool mentorSwitchActivate;
+
+	// Use this for initialization
+	void Start () {
+        mentorSwitchActivate = false;
+	}
+
+    void ActivateSwitch()
+    {
+        // activate the mentor's switch
+        mentorSwitchActivate = true;
+    }
+}
